176e77bf1493a4cbdc822546b9993fd81ecab103
Axel V. <axelvaillantiris@gmail.com>
2026-03-26 14:57:04 +0100

    Merge pull request #1 from axdevmin/feature/add-map-animated
    Feature/add map animated

---
+++ src/hooks/use-select-file-dialog.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+
+type ShowFileDialogFunction = () => void;
+type OnSelectFileFunction = (file: File) => void;
+
+export const useSelectFileDialog = (
+  onSelect: OnSelectFileFunction
+): [React.ReactNode, ShowFileDialogFunction] => {
+  const ref = React.useRef<HTMLInputElement>(null);
+
+  const onChange = React.useCallback(
+    (ev: React.ChangeEvent<HTMLInputElement>) => {
+      if (!ev.currentTarget.files) return;
+      const file = ev.currentTarget.files[0] as File | undefined;
+      ev.currentTarget.value = "";
+      if (file) {
+        onSelect(file);
+      }
+    },
+    [onSelect]
+  );
+
+  const node = React.useMemo(
+    () => (
+      <input
+        type="file"
+        accept=".jpeg,.jpg,.svg,.png,.gif,.mp4,.webm,.ogg,.mov,.zip,.md,.markdown"
+        ref={ref}
+        onChange={onChange}
+        style={{ display: "none" }}
+      />
+    ),
+    [onChange]
+  );
+
+  const showFileDialog = React.useCallback(() => {
+    ref.current?.click();
+  }, []);
+
+  return [node, showFileDialog];
+};
